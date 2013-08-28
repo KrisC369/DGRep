72749bc4475ad1489373b4a827e32956c3e70aff
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2013-08-28 18:37:34 +0200

    Cleaned up configuration class to conform to style rules.

--- src/main/java/be/kriscon/dgrep/config/Configuration.java
+++ src/main/java/be/kriscon/dgrep/config/Configuration.java
@@ -1,37 +1,38 @@
 package be.kriscon.dgrep.config;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 /**
- * 
+ * Format and API a configuration object should adhere to.
+ *
  * @author Kristof Coninx <kristof.coninx@student.kuleuven.be>
  * @credit Rutger Claes <rutger.claes@cs.kuleuven.be>
  */
 public interface Configuration {
 
-    public double getDouble( String key, double defaultValue );
+    double getDouble(String key, double defaultValue);
 
-    public float getFloat( String key, float defaultValue );
+    float getFloat(String key, float defaultValue);
 
-    public long getLong( String key, long defaultValue );
+    long getLong(String key, long defaultValue);
 
-    public int getInt( String key, int defaultValue );
+    int getInt(String key, int defaultValue);
 
-    public short getShort( String key, short defaultValue );
+    short getShort(String key, short defaultValue);
 
-    public boolean getBoolean( String key, boolean defaultValue );
+    boolean getBoolean(String key, boolean defaultValue);
 
-    public String getString( String key, String defaultValue );
+    String getString(String key, String defaultValue);
 
-    public Iterator<String> getKeys();
+    Iterator<String> getKeys();
 
-    public List<?> getList( String key );
+    List<?> getList(String key);
 
-    public <T> List<T> getList( String key, List<T> defaultValue );
+    <T> List<T> getList(String key, List<T> defaultValue);
 
-    public Configuration subset( String prefix );
+    Configuration subset(String prefix);
 
-    public Map<Object,Object> asMap();
-
+    Map asMap();
 }
